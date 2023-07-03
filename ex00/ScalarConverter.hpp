5759ff3119ad9b8df2d0be57d686181feea02853
Mars <almasnorazman2803@outlook.com>
2023-07-03 19:32:48 +0800

    help'

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter {
+	private:
+		const std::string _literal;
+		char _char;
+		int _int;
+		float _float;
+		double _double;
+		ScalarConverter( void );
+
+	public:
+		ScalarConverter( std::string lit );
+		ScalarConverter( const ScalarConverter &copy );
+		ScalarConverter &operator=( const ScalarConverter &assign );
+		~ScalarConverter( void );
+
+		void convertInput( std::string str );
+		void printResults( void );
+};
+#endif
